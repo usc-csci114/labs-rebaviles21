e1324b95be71893ea07be852e2b4bbe829f5c163
Andrew Goodney <goodney@usc.edu>
2024-09-15 16:25:23 -0700

    add lab3

---
+++ lab3/ArrayList.h
@@ -0,0 +1,71 @@
+#ifndef __ARRAYLIST_H__
+#define __ARRAYLIST_H__
+#include <stdexcept>
+#include "List.h"
+
+template <typename T>
+class ArrayList : public List<T> {
+public:
+	ArrayList();
+	void append(T v);
+	void insert(size_t idx, T v);
+	T at(size_t idx);
+	void remove(size_t idx);
+	size_t find(T v);
+	size_t size();
+	bool empty();
+
+private:
+	void resize(); //double the size of the array and copy the items
+	size_t m_capacity;
+	size_t m_size;
+	T* data;
+};
+
+template <typename T>
+ArrayList<T>::ArrayList()
+{
+	
+}
+
+template <typename T>
+void ArrayList<T>::append(T v)
+{
+
+}
+
+template <typename T>
+void ArrayList<T>::insert(size_t idx, T v)
+{
+	
+}
+
+template <typename T>
+T ArrayList<T>::at(size_t idx)
+{
+	
+}
+
+template <typename T>
+void ArrayList<T>::remove(size_t idx)
+{
+	
+}
+template <typename T>
+size_t ArrayList<T>::find(T v)
+{
+	
+}
+
+template <typename T>
+size_t ArrayList<T>::size()
+{
+	
+}
+
+template <typename T>
+bool ArrayList<T>::empty()
+{
+	
+}
+#endif
---
+++ lab3/List.h
@@ -0,0 +1,15 @@
+#ifndef __LIST_H__
+#define __LIST_H__
+template <typename T>
+class List {
+public:
+	virtual void append(T v)=0;
+	virtual void insert(size_t idx, T v)=0;
+	virtual T at(size_t idx)=0;
+	virtual void remove(size_t idx)=0;
+	virtual size_t find(T v)=0;
+	virtual size_t size()=0;
+	virtual bool empty()=0;
+};
+
+#endif
